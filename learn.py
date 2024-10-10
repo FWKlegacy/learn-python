b43d8a67ac4491a274fc7dc309d3e6bd9a4b8b34
Brevian <Wanjalawafulabrevian@gmail.com>
2024-10-10 15:49:36 +0300

    python learning

---
+++ learn.py
@@ -0,0 +1,16 @@
+if 5>2:
+  print("five is greater than 2")
+  x= "john"
+  y= 5
+  print(x)
+  print(y)
+  my_Name= "john"
+  my_num=5
+  print(my_Name)
+  print(my_num)
+  myvar="brevian"
+  print(myvar)
+  myNameIs="waffula"
+  print(myNameIs)
+  my_name_is="bravo"
+  print(my_name_is)
