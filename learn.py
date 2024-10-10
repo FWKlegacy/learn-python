bdac608b4acdb831948070094ce925f84b2c243a
Brevian <Wanjalawafulabrevian@gmail.com>
2024-10-10 17:34:03 +0300

    pushing my code

--- learn.py
+++ learn.py
@@ -14,3 +14,60 @@ if 5>2:
   print(myNameIs)
   my_name_is="bravo"
   print(my_name_is)
+
+
+  fruits=["mangos","oranges","avocados"]
+  x,y,z= fruits
+  print(x)
+  print(y)
+  print(z)
+
+
+  a= "awesome"
+
+def myfunc():
+    print("kenya is "+ a)
+
+myfunc()
+
+
+
+x= "awesome"
+
+
+def func():
+ x="fantastic"
+ print("kenya is " +  x)
+
+func()
+print("kenya is "+  x)
+
+def myfunc():
+    x="john doe"
+    print(x)
+myfunc()
+
+def myfunc():
+    global x
+    x="fantastic"
+myfunc()
+print("kenya is " +x)
+
+
+
+x="kazi poa"
+
+def myfunc():
+    x="form ni kutesa"
+myfunc()
+print("kenya " + x)
+
+
+x="kazi poa"
+
+def myfunc():
+    global x
+    x="form ni kutesa"
+myfunc()
+print("kenya " + x)
+
