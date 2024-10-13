9e52a320e20bca5ae0d4e4f986daf42700b3b0d6
Brevian <Wanjalawafulabrevian@gmail.com>
2024-10-13 11:10:32 +0300

    f

--- learn.py
+++ learn.py
@@ -222,3 +222,13 @@ while i <20:
     if i ==10:
       break
     i+=1
+
+#Python Functions(A function is a block of code which only runs when it is called.
+
+#You can pass data, known as parameters, into a function.
+
+#A function can return data as a result.)
+#Arguments
+#Information can be passed into functions as arguments.
+
+
